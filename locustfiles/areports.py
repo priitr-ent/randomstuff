3251d1453ae2a269bb1bd9bc7284871381a57539
Priit Randla <priit.randla@entigo.com>
2020-11-27 15:50:08 +0200

    .

---
+++ locustfiles/areports.py
@@ -0,0 +1,33 @@
+import random
+import json
+from locust import HttpUser,SequentialTaskSet, User, TaskSet, task, between
+
+
+class Heavypdf(TaskSet):
+    @task
+    def pages(self):
+        self.client.get("/api/v3/templates/24839/output?name=LoadTesting&format=pdf&output=base64&data=https://pdfapi-test-data-bucket.s3.amazonaws.com/large_invoice_test.json&key=e2b73c33ebc402fcaa05205de569a43c&secret=e45c2b47cdff177ab5a4a46910fcee1e&workspace=info@actualreports.com")
+
+class Frontpage(TaskSet):
+    @task
+    def pages(self):
+        self.client.get("/")
+
+class Price(TaskSet):
+    @task
+    def pages(self):
+        self.client.get("/pricing")
+
+class Invoice(TaskSet):
+    @task
+    def pages(self):
+        self.client.get("/api/v3/templates/24386/output?name=LoadTesting&format=pdf&output=base64&data=https://pdfgeneratorapi.com/example-documents/34873/json&key=61e5f04ca1794253ed17e6bb986c1702&secret=68db1902ad1bb26d34b3f597488b9b28&workspace=demo.example@actualreports.com")
+
+class WebUsers(HttpUser):
+    email = "NOT_FOUND"
+    password = "NOT_FOUND"
+    name = "NOT_FOUND"
+    headers = {'content-type': 'application/json'}
+    wait_time = between(1, 1)
+    weight = 1
+    tasks = {Heavypdf:0, Frontpage:1, Price:1, Invoice:1}
