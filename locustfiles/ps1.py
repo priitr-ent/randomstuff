60729050149b751588bc099b774c8ee14056cb61
Priit Randla <priit.randla@entigo.com>
2021-10-25 13:21:58 +0300

    py1 2

--- locustfiles/ps1.py
+++ locustfiles/ps1.py
@@ -1,6 +1,9 @@
 from locust import HttpUser, task
 
 class HelloWorldUser(HttpUser):
+    def on_start(self):
+        self.client.headers = {'SFSESSID': 'bpunmg3rp6t9ktqpp8jt4iblqq'}
+
     @task
     def hello_world(self):
         self.client.get("/")
